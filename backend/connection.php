152f23f9cc3ed105ee1fa32ea0c1c2cc9853cc26
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-13 23:36:38 +0300

    Created DB, Added Connection Api

---
+++ backend/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = "toor";
+$db_name = "bootstrap-clone";
+
+$mysql = new mysqli($host, $db_user, $db_pass, $db_name);
+
+?>
--- backend/post-contact.php
+++ backend/post-contact.php
@@ -7,7 +7,7 @@ $userEmail = $_POST["userEmail"];
 $userPhone = $_POST["userPhone"];
 $userMessage = $_POST["userMessage"];
 
-$query = $mysqli -> prepare("INSERT INTO contacts(name, email, phone, message) VALUE (?, ?, ?,  ?)");
+$query = $mysql -> prepare("INSERT INTO contacts(name, email, phone, message) VALUE (?, ?, ?,  ?)");
 $query -> bind_param("ssss", $userName, $userEmail, $userPhone, $userMessage);
 $query -> execute();
 
