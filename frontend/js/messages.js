aed5b535f061c9e949b0870a8970e81a830fc04e
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-14 12:53:51 +0300

    Stylized Table, Readable Output

--- frontend/js/messages.js
+++ frontend/js/messages.js
@@ -12,11 +12,11 @@ function getResponse() {
         let output = "";
         for(let i in data) {
             output += `<tr>
-                <td>${data[i].id}</td>
-                <td>${data[i].name}</td>
-                <td>${data[i].email}</td>
-                <td>${data[i].phone}</td>
-                <td class="table-wrap">${data[i].message}</td>
+                <td class="add-border">${data[i].id}</td>
+                <td class="add-border">${data[i].name}</td>
+                <td class="add-border">${data[i].email}</td>
+                <td class="add-border">${data[i].phone}</td>
+                <td class="table-wrap add-border">${data[i].message}</td>
             </tr>`;
         }
 
